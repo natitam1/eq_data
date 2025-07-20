3b8509f9dc90d7dd01d5f9d34dfe303dc18c494d
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-20 17:13:43 +0300

    Add the strength of magnitude

--- eq_world_map.py
+++ eq_world_map.py
@@ -4,7 +4,7 @@ import json
 import plotly.express as px
 
 # Read data as a string and convert to a python object.
-path = Path('eq_data_1_day_m1.geojson')
+path = Path('eq_data_30_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
@@ -21,5 +21,5 @@ for eq_dict in all_eq_dicts:
     lats.append(lat)
 
 title = 'Global Earthquakes'
-fig = px.scatter_geo(lat=lats, lon=lons, title=title)
+fig = px.scatter_geo(lat=lats, lon=lons, size=mags, title=title)
 fig.write_html('world_earthquake.html')
